0e1c89eee8592a244eb8fc7730cb3c13fedd3f60
Michael Ballantyne <michael.ballantyne@gmail.com>
2015-06-26 15:46:52 -0600

    Add appendo to numbers.scm

--- numbers.scm
+++ numbers.scm
@@ -1,3 +1,12 @@
+(define appendo
+  (lambda (l s out)
+    (conde
+      [(== '() l) (== s out)]
+      [(fresh (a d res)
+         (== `(,a . ,d) l)
+         (== `(,a . ,res) out)
+         (appendo d s res))])))
+
 (define build-num
   (lambda (n)
     (cond
