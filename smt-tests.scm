b2a0f98fc8051695e269b077b4e4bd9af22c7fc9
Nada Amin <namin@seas.harvard.edu>
2019-09-17 17:25:13 -0400

    add abstract interpretation tests

--- smt-tests.scm
+++ smt-tests.scm
@@ -4,4 +4,6 @@
 (load "test-check.scm")
 
 (load "../clpsmt-miniKanren/z3-driver.scm")
+;;(load "../clpsmt-miniKanren/cvc4-driver.scm")
 (load "../clpsmt-miniKanren/talk.scm")
+(load "../clpsmt-miniKanren/radi-tests.scm")
