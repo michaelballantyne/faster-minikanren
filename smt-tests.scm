fdad309d380ed4b990c2c7ac73f2540738c536bb
Nada Amin <namin@alum.mit.edu>
2019-09-17 19:26:31 -0400

    more tests

--- smt-tests.scm
+++ smt-tests.scm
@@ -9,3 +9,8 @@
 (load "../clpsmt-miniKanren/radi-tests.scm")
 (load "../clpsmt-miniKanren/radiw-concrete-tests.scm")
 ;;(load "../clpsmt-miniKanren/full-abstract-interp-extended-tests.scm")
+(load "../clpsmt-miniKanren/rsa.scm")
+(load "../clpsmt-miniKanren/property-based-synthesis-tests.scm")
+;;slow
+;;(load "../clpsmt-miniKanren/twenty-four-puzzle.scm")
+
