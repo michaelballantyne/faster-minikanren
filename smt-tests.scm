315b0a81ab794bae7c5b0a12953d1cb4fbe55238
Nada Amin <namin@alum.mit.edu>
2019-09-17 20:04:17 -0400

    routine

--- smt-tests.scm
+++ smt-tests.scm
@@ -1,10 +1,11 @@
 (load "mk-vicare.scm")
 (load "mk.scm")
 (load "smt.scm")
-(load "test-check.scm")
+(load "../clpsmt-miniKanren/test-check.scm")
 
 (load "../clpsmt-miniKanren/z3-driver.scm")
 ;;(load "../clpsmt-miniKanren/cvc4-driver.scm")
+;;(load "../clpsmt-miniKanren/z3-server.scm")
 (load "../clpsmt-miniKanren/talk.scm")
 (load "../clpsmt-miniKanren/radi-tests.scm")
 (load "../clpsmt-miniKanren/radiw-concrete-tests.scm")
@@ -13,4 +14,3 @@
 (load "../clpsmt-miniKanren/property-based-synthesis-tests.scm")
 ;;slow
 ;;(load "../clpsmt-miniKanren/twenty-four-puzzle.scm")
-
