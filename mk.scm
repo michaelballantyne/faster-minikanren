5f102e2d86302582e5e80421fcb39d9002427060
Michael Ballantyne <michael.ballantyne@gmail.com>
2025-02-04 16:50:00 -0700

    fix variable allocation order in Chez

--- mk.scm
+++ mk.scm
@@ -329,8 +329,11 @@
      (lambda (st)
        (suspend
          (let ((scope (subst-scope (state-S st))))
-           (let ((x (var scope)) ...)
-             (bind* (g0 st) g ...))))))))
+           ;; Use let* to force left-to-right variable allocation order
+           (let* ((x (var scope)) ...)
+             ;; Use let to get a duplicate binding error.
+             (let ((x x) ...)
+               (bind* (g0 st) g ...)))))))))
 
 ; (conde [g:Goal ...] ...+) -> Goal
 (define-syntax conde
